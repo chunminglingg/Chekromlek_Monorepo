8a49b1d622edf78342d6603b36609177a4551ec6
BravoGod <killermr86@gmail.com>
2024-06-12 23:39:46 +0700

    debug post listing

--- packages/api-gateway/src/middleware/unless-route.ts
+++ packages/api-gateway/src/middleware/unless-route.ts
@@ -1,10 +1,21 @@
 import { NextFunction, Request, Response } from "express";
 
+// Middleware to conditionally apply another middleware unless the route matches specific paths and methods
+interface Condition {
+  path: string;
+  method?: string;
+}
 
-// Middleware to conditionally apply another middleware unless the route matches a specific path
-export default function unless(path: string, middleware: Function) {
+export default function unless(conditions: Condition[], middleware: Function) {
   return (req: Request, res: Response, next: NextFunction) => {
-    if (req.path.startsWith(path)) {
+    const shouldSkip = conditions.some((condition) => {
+      const pathMatches = req.path.startsWith(condition.path);
+      const methodMatches =
+        !condition.method || req.method === condition.method;
+      return pathMatches && methodMatches;
+    });
+
+    if (shouldSkip) {
       return next();
     } else {
       middleware(req, res, next);
