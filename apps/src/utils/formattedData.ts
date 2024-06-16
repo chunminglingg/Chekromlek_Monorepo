8fde7f77c6e88145f50b01140bb971fb8fff481b
navin@1503 <79439418+khievnavin@users.noreply.github.com>
2024-06-16 19:20:14 +0700

    Update formattedData.ts

--- apps/src/utils/formattedData.ts
+++ apps/src/utils/formattedData.ts
@@ -1,50 +1,31 @@
-const calculateElapsedTime = (isoString: string) => {
-  const date = new Date(isoString);
-  const now = new Date();
+// const calculateElapsedTime = (isoString: string) => {
+//   const date = new Date(isoString);
+//   const now = new Date();
 
-  // Calculate elapsed time in seconds
-  const secondsElapsed = Math.floor((now.getTime() - date.getTime()) / 1000);
+//   // Calculate elapsed time in seconds
+//   const secondsElapsed = Math.floor((now.getTime() - date.getTime()) / 1000);
 
-  // Determine the appropriate string based on elapsed time
-  if (secondsElapsed < 60) {
-    return `${secondsElapsed} second${secondsElapsed !== 1 ? "s" : ""} ago`;
-  } else if (secondsElapsed < 3600) {
-    const minutesElapsed = Math.floor(secondsElapsed / 60);
-    return `${minutesElapsed} minute${minutesElapsed !== 1 ? "s" : ""} ago`;
-  } else if (secondsElapsed < 86400) {
-    const hoursElapsed = Math.floor(secondsElapsed / 3600);
-    return `${hoursElapsed} hour${hoursElapsed !== 1 ? "s" : ""} ago`;
-  } else if (secondsElapsed < 2592000) {
-    // 30 days in seconds
-    const daysElapsed = Math.floor(secondsElapsed / 86400);
-    return `${daysElapsed} day${daysElapsed !== 1 ? "s" : ""} ago`;
-  } else if (secondsElapsed < 31536000) {
-    // 365 days in seconds
-    const monthsElapsed = Math.floor(secondsElapsed / 2592000); // 30 days in seconds
-    return `${monthsElapsed} month${monthsElapsed !== 1 ? "s" : ""} ago`;
-  } else {
-    const yearsElapsed = Math.floor(secondsElapsed / 31536000); // 365 days in seconds
-    return `${yearsElapsed} year${yearsElapsed !== 1 ? "s" : ""} ago`;
-  }
-};
-
-export const formattedData = (data: any) => {
-  return data.map((item: any) => {
-    return {
-      ...item,
-      id: item._id,
-      createdAt: calculateElapsedTime(item.createdAt),
-    };
-  });
-};
-
-
-// import { format, parseISO } from 'date-fns';
-
-// const formatDate = (isoString: string) => {
-//   const date = parseISO(isoString);
-//   const formattedDate = format(date, "MM/dd/yyyy, HH:mm:ss");
-//   return formattedDate;
+//   // Determine the appropriate string based on elapsed time
+//   if (secondsElapsed < 60) {
+//     return `${secondsElapsed} second${secondsElapsed !== 1 ? "s" : ""} ago`;
+//   } else if (secondsElapsed < 3600) {
+//     const minutesElapsed = Math.floor(secondsElapsed / 60);
+//     return `${minutesElapsed} minute${minutesElapsed !== 1 ? "s" : ""} ago`;
+//   } else if (secondsElapsed < 86400) {
+//     const hoursElapsed = Math.floor(secondsElapsed / 3600);
+//     return `${hoursElapsed} hour${hoursElapsed !== 1 ? "s" : ""} ago`;
+//   } else if (secondsElapsed < 2592000) {
+//     // 30 days in seconds
+//     const daysElapsed = Math.floor(secondsElapsed / 86400);
+//     return `${daysElapsed} day${daysElapsed !== 1 ? "s" : ""} ago`;
+//   } else if (secondsElapsed < 31536000) {
+//     // 365 days in seconds
+//     const monthsElapsed = Math.floor(secondsElapsed / 2592000); // 30 days in seconds
+//     return `${monthsElapsed} month${monthsElapsed !== 1 ? "s" : ""} ago`;
+//   } else {
+//     const yearsElapsed = Math.floor(secondsElapsed / 31536000); // 365 days in seconds
+//     return `${yearsElapsed} year${yearsElapsed !== 1 ? "s" : ""} ago`;
+//   }
 // };
 
 // export const formattedData = (data: any) => {
@@ -52,7 +33,32 @@ export const formattedData = (data: any) => {
 //     return {
 //       ...item,
 //       id: item._id,
-//       createdAt: formatDate(item.createdAt),
+//       createdAt: calculateElapsedTime(item.createdAt),
 //     };
 //   });
 // };
+
+
+const formatDate = (isoString: string) => {
+  const date = new Date(isoString);
+  const formattedDate = date.toLocaleString("en-US", {
+    timeZone: "Asia/Phnom_Penh",
+    // year: "numeric",
+    // month: "2-digit",
+    // day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
+  return formattedDate;
+};
+
+export const formattedData = (data: any) => {
+  return data.map((item: any) => {
+    return {
+      ...item,
+      id: item._id,
+      createdAt: formatDate(item.createdAt),
+    };
+  });
+};
